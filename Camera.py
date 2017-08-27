3717eb40e62e542ef7534761210e89fd3cea9534
bubble6 <1456816124@qq.com>
2017-08-27 11:26:44 +0800

    增加USB摄像头的拍照功能 (#27)
    * 	增加USB摄像头的拍照功能
    
    * 增加USB摄像头的拍照功能
    
    * 	增加USB摄像头的拍照功能
    
    * 	增加USB摄像头的拍照功能
    
    * 增加USB摄像头的拍照功能
    
    * 增加USB摄像头的拍照功能
    
    * 增加USB摄像头的拍照功能

--- Camera.py
+++ Camera.py
@@ -84,9 +84,8 @@ def handle(text, mic, profile, wxbot=None):
                 command.append('-hf')
         if sound and count_down > 0:
             mic.say(u"收到，%d秒后启动拍照" % (count_down))
-            if usb_camera:
-                time.sleep(count_down)
-
+            if usb_camera: time.sleep(count_down)
+            
         process = subprocess.Popen(command, shell=usb_camera)
         res = process.wait()
         if res != 0:
