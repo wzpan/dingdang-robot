1868fc95f6c8e22d7ee25d7f2910575425ebb373
Deschanel <201400150159@mail.sdu.edu.cn>
2017-09-16 10:00:28 +0800

    支持在HomeAssistant中增加叮当专用控制字段 (#44)
    * 增加homeassistant，当前只是测试阶段，目前只添加了sensor和switch功能
    
    * Delete Hass.py
    
    * 增加HomeAssistant
    
    * 更改缩进
    
    * Delete Hass.py
    
    * 新增Homeassistant
    
    * Delete Hass.py
    
    * 新增Homeassistant
    
    * Update Hass.py
    
    * Delete Hass.py
    
    * 新增Homeassistant
    
    * Delete Hass.py
    
    * 新增Homeassistant
    
    * 新的更改
    
    * 新的更改
    
    * 新的更改
    
    * 增加叮当控制字段
    
    * 修改格式

--- client/plugins/Hass.py
+++ client/plugins/Hass.py
@@ -17,12 +17,13 @@ def handle(text, mic, profile, wxbot=None):
     while not input:
         mic.say(u"请重新说")
         input = mic.activeListen(MUSIC=True)
+    input = input.split(",")[0].split("，")[0]
     hass(input, mic, profile)
 
 
 def hass(text, mic, profile):
     logger = logging.getLogger(__name__)
-    if profile[SLUG] or 'url' not in profile[SLUG] or \
+    if not profile[SLUG] or 'url' not in profile[SLUG] or \
        'port' not in profile[SLUG] or \
        'password' not in profile[SLUG]:
         mic.say(u"主人配置有误")
@@ -44,43 +45,41 @@ def hass(text, mic, profile):
     for device in devices:
         name = device["attributes"]["friendly_name"]
         state = device["state"]
-        if name in text:
-            device_domain = device["entity_id"].split(".")[0]
-            if device_domain == "sensor" or not isAction(text):
-                try:
-                    measurement = device["attributes"]["unit_of_measurement"]
-                except Exception, e:
-                    pass
-                if 'measurement' in locals().keys():
-                    text = text + "状态是" + state + measurement
-                    mic.say(text)
-                else:
-                    text = text + "状态是" + state
-                    mic.say(text)
-            elif device_domain == "switch" and isAction(text):
-                try:
-                    if any(word in text for word in [u"开始", u"打开", u"开启"]):
-                        newS = "turn_on"
-                    elif any(word in text for word in [u"停止", u"结束", u"退出"]):
-                        newS = "turn_off"
-                    p = json.dumps({"entity_id": device["entity_id"]})
-                    url_s = url + ":" + port + "/api/services/switch/" + newS
-                    request = requests.post(url_s, headers=headers, data=p)
-                    if format(request.status_code) == "200" or \
-                       format(request.status_code) == "201":
-                        mic.say(u"执行成功")
+        attributes = device["attributes"]
+        if 'dingdang' in attributes.keys():
+            dingdang = attributes["dingdang"]
+            if isinstance(dingdang, list):
+                if text in dingdang:
+                    try:
+                        measurement = attributes["unit_of_measurement"]
+                    except Exception, e:
+                        pass
+                    if 'measurement' in locals().keys():
+                        text = text + "状态是" + state + measurement
+                        mic.say(text)
                     else:
-                        mic.say(u"对不起,执行失败")
-                        print(format(request.status_code))
-                except Exception, e:
-                    pass
-        break
+                        text = text + "状态是" + state
+                        mic.say(text)
+                    break
+            elif isinstance(dingdang, dict):
+                if text in dingdang.keys():
+                    try:
+                        act = dingdang[text]
+                        p = json.dumps({"entity_id": device["entity_id"]})
+                        s = "/api/services/switch/"
+                        url_s = url + ":" + port + s + act
+                        request = requests.post(url_s, headers=headers, data=p)
+                        if format(request.status_code) == "200" or \
+                           format(request.status_code) == "201":
+                            mic.say(u"执行成功")
+                        else:
+                            mic.say(u"对不起,执行失败")
+                            print(format(request.status_code))
+                    except Exception, e:
+                        pass
+                    break
     else:
         mic.say(u"对不起,指令不存在")
-
-
-def isAction(text):
-    return any(word in text for word in ["打开", "停止", "结束", "开始"])
 
 
 def isValid(text):
