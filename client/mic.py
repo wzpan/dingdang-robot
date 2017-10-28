74069718955644f54bd8362c0a01e2a0eef070bf
Joseph Pan <m@hahack.com>
2017-10-28 12:58:35 +0800

    增加 exception_on_overflow 的选项

--- client/mic.py
+++ client/mic.py
@@ -270,7 +270,7 @@ class Mic:
         """
 
         RATE = 16000
-        CHUNK = 8192
+        CHUNK = 1024
         LISTEN_TIME = 12
 
         # check if no threshold provided
@@ -293,7 +293,7 @@ class Mic:
 
         for i in range(0, RATE / CHUNK * LISTEN_TIME):
             try:
-                data = stream.read(CHUNK)
+                data = stream.read(CHUNK, exception_on_overflow=False)
                 frames.append(data)
                 score = self.getScore(data)
 
