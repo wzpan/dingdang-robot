c3ecf77a33cb1ec7e520821fce95f7c8775fab26
Joseph Pan <m@hahack.com>
2017-11-05 22:07:20 +0800

    修复启动脚本中的一些问题

--- launcher/dingdang-launcher-root.sh
+++ launcher/dingdang-launcher-root.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 sleep 1
 
+session_name=dingdang
+
 #Delete Cache
 sudo rm -r /root/.cache
 sudo rm -r /root/.netease-musicbox
 sudo rm -r /root/userInfo
 
 #Restore Configuration of AlsaMixer
-if [ -f /home/pi/asound.state ]; then
-    alsactl --file=/home/pi/asound.state restore
+if [ -f $HOME/asound.state ]; then
+    alsactl --file=$HOME/asound.state restore
     sleep 1
 fi
 
@@ -16,6 +18,6 @@ fi
 sudo tmux new-session -d -s $session_name $HOME/dingdang/dingdang.py
 sleep 1
 
-if [ -d /home/pi/ReSpeaker-Switcher ]; then
-    sudo python /home/pi/ReSpeaker-Switcher/switcher.py &
+if [ -d $HOME/ReSpeaker-Switcher ]; then
+    python $HOME/ReSpeaker-Switcher/switcher.py &
 fi
